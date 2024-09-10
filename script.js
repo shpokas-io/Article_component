c85e8472d53db168b82720f08f7a5bddc8ea29c6
shpokas270 <s.shpokas@gmail.com>
2024-09-10 10:44:56 +0300

    Fixed typo mistakes in js script

--- script.js
+++ script.js
@@ -7,4 +7,28 @@ document.addEventListener(`DOMContentLoaded`, () => {
   shareButton.addEventListener(`click`, () => {
     shareButton.classList.toggle(`active`);
   });
+
+  //Toggle visible share option
+  if (shareButton.classList.contains(`active`)) {
+    shareOptions.style.display = `flex`; //bscly addded anything to unhide hiden element
+    setTimeout(() => {
+      shareOptions.style.opacity = `0`;
+      shareOptions.style.visibility = "hidden";
+      shareOptions.style.bottom = `100%`;
+    }, 10);
+  } else {
+    shareOptions.style.opacity = `0`;
+    shareOptions.style.visibility = `hidden`;
+    shareOptions.style.bottom = `-100%`;
+
+    shareOptions.addEventListener(
+      `transitionend`,
+      () => {
+        if (!shareButton.classList.contains(`active`)) {
+          shareOptions.style.display = `none`;
+        }
+      },
+      { once: true }
+    );
+  }
 });
